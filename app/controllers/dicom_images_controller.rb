eafa0e8bffc83492777b6014a2652198e66604e9
Claire Janke <clairejanke@gmail.com>
2024-02-14 21:17:07 -0500

    Support POST request to store image

--- app/controllers/dicom_images_controller.rb
+++ app/controllers/dicom_images_controller.rb
@@ -1,10 +1,23 @@
 class DicomImagesController < ApplicationController
   def create
+    @dicom_image = DicomImage.new(dicom_image_params)
+
+    if @dicom_image.save
+      render json: @dicom_image, status: :created
+    else
+      render json: {error: 'Dicom image upload failed'}, status: :unprocessable_entity
+    end
   end
 
   def show
   end
 
   def elements
+  end
+
+  private
+
+  def dicom_image_params
+    params.permit(:raw, :filename)
   end
 end
